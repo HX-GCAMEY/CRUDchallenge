f555a9c7a983de0c06cdec6e83eb477e289ba789
Gama <lic.camey@gmail.com>
2023-04-12 17:32:29 -0300

    initial commit

---
+++ src/controllers/deleteControllers.js
@@ -0,0 +1,21 @@
+const products = require("../utils/data.json");
+
+const path = require("path");
+const fsPromises = require("fs/promises");
+
+const dataFilePath = path.join(process.cwd(), "/src/utils/data.json");
+
+const deleteProduct = async (id) => {
+  const found = products.find((product) => product.id === Number(id));
+  if (found) {
+    const deletion = products.filter((elements) => elements.id !== found.id);
+
+    const updatedData = JSON.stringify(deletion);
+
+    await fsPromises.writeFile(dataFilePath, updatedData);
+
+    return products;
+  }
+};
+
+module.exports = {deleteProduct};
